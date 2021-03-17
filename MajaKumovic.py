474d2f15755fc7e22ba8065e91a201c93f89aff3
Maja Kumovic <majakumovic@gmail.com>
2021-03-17 10:18:34 +0100

    Maja mjenja fajl

--- MajaKumovic.py
+++ MajaKumovic.py
@@ -1,3 +1,18 @@
 #kineski horoskop
 
-astroloske_zivotinje =['pacov', 'vo', 'tigar', 'zec', 'zmaj', 'zmija','konj','koza', 'majmun', 'pjetao', 'pas', 'svinja']
+astroloske_zivotinje =['pacov', 'vo', 'tigar', 'zec', 'zmaj', 'zmija',
+'konj','koza', 'majmun', 'pjetao', 'pas', 'svinja']
+
+godinaRodjenja = int(input("Unesite godinu rodjenja:"))
+def astroloskiZnak(godinaRodjenja):
+    if godinaRodjenja > 1900 and godinaRodjenja < 2022:
+        brojCiklusa = (godinaRodjenja - 1900) % 12
+        poruka = astroloske_zivotinje[brojCiklusa]
+        return poruka
+
+    else:
+        poruka = "Greška"
+        return poruka
+       
+odgovor = astroloskiZnak(godinaRodjenja)
+print(odgovor)
